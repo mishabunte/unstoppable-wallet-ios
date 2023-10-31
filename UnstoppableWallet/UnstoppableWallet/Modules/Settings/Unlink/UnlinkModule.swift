14f9be787eb2376a8176d353d51f66dbbcb964f8
Misha Bunte <github@bunte.art>
2023-10-30 21:05:51 -0700

    Add hardware wallet unlink UI

--- UnstoppableWallet/UnstoppableWallet/Modules/Settings/Unlink/UnlinkModule.swift
+++ UnstoppableWallet/UnstoppableWallet/Modules/Settings/Unlink/UnlinkModule.swift
@@ -5,7 +5,16 @@ struct UnlinkModule {
     static func viewController(account: Account) -> UIViewController {
         let service = UnlinkService(account: account, accountManager: App.shared.accountManager)
         let viewModel = UnlinkViewModel(service: service)
-        let viewController = account.watchAccount ? UnlinkWatchViewController(viewModel: viewModel) : UnlinkViewController(viewModel: viewModel)
+        let viewController : UIViewController = {
+            if account.watchAccount {
+                return UnlinkWatchViewController(viewModel: viewModel)
+            } else if account.hardwareAccount {
+                return UnlinkHardwareViewController(viewModel: viewModel)
+            } else {
+                return UnlinkViewController(viewModel: viewModel)
+            }
+        }()
+
         return viewController.toBottomSheet
     }
 
